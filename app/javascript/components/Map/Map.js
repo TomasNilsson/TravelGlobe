eeced6e5ba26660f9e175d394ce33bd13d5e05dd
Tomas Nilsson <tomas.nilsson92@gmail.com>
2020-07-17 17:43:42 +0200

    Apply custom TravelGlobe styles to the map

--- app/javascript/components/Map/Map.js
+++ app/javascript/components/Map/Map.js
@@ -24,6 +24,78 @@ const Map = ({ center = { lat: 30, lng: 25 }, zoom = 3, options = {} }) => (
         },
         strictBounds: true,
       },
+      styles: [
+        {
+          featureType: 'all',
+          elementType: 'all',
+          stylers: [{ hue: '#3b5998' }, { saturation: -22 }],
+        },
+        {
+          featureType: 'administrative',
+          elementType: 'labels.icon',
+          stylers: [{ visibility: 'on' }, { color: '#3b5998' }],
+        },
+        {
+          featureType: 'administrative.country',
+          elementType: 'geometry.stroke',
+          stylers: [{ visibility: 'simplified' }, { color: '#3b5998' }],
+        },
+        {
+          featureType: 'landscape',
+          elementType: 'geometry',
+          stylers: [
+            { visibility: 'on' },
+            { color: '#f7f7f7' },
+            { saturation: 10 },
+            { lightness: 76 },
+          ],
+        },
+        {
+          featureType: 'landscape.natural',
+          elementType: 'geometry',
+          stylers: [{ color: '#f7f7f7' }],
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'all',
+          stylers: [{ color: '#8b9dc3' }, { visibility: 'simplified' }],
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.icon',
+          stylers: [{ visibility: 'off' }],
+        },
+        {
+          featureType: 'road.local',
+          elementType: 'geometry.fill',
+          stylers: [{ color: '#8b9dc3' }],
+        },
+        {
+          featureType: 'transit.line',
+          elementType: 'all',
+          stylers: [{ color: '#ffffff' }, { weight: 0.43 }],
+        },
+        {
+          featureType: 'transit',
+          elementType: 'labels',
+          stylers: [{ visibility: 'off' }],
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{ color: '#3b5998' }],
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels',
+          stylers: [
+            { color: '#3b5998' },
+            { saturation: 10 },
+            { lightness: 30 },
+            { weight: 0.43 },
+          ],
+        },
+      ],
       ...options,
     }}
   />
