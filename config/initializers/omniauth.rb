332d46217761d79dd4aa541fdbea852b3a3f8dc9
Tomas Nilsson <tomas.nilsson92@gmail.com>
2020-08-02 15:22:12 +0200

    Use latest version (v7.0) of Facebook API

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,9 @@ OmniAuth.config.on_failure = Proc.new { |env|
 }
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], secure_image_url: true
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], secure_image_url: true,  
+    client_options: {
+      site: 'https://graph.facebook.com/v7.0',
+      authorize_url: "https://www.facebook.com/v7.0/dialog/oauth"
+    }
 end
